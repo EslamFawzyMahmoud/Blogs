c3934360370774fce2731eb3eada21ceb8cb96f7
eslam_fawzy <eslamfawzy103@gmail.com>
2020-12-29 23:00:36 +0200

    Add Model For Post

---
+++ Post/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+# title -user -image-body-Created-content
+
+class Post(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.CharField(max_length=50)
+    image=models.ImageField(upload_to='post_img/')
+    conrtent=models.TextField(default=' ')
+    created=models.DateTimeField()
+
+    def __str__(self):
+        return self.title
